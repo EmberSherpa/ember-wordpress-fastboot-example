82e0497d5e429cc8c7255d0bbec839082205c39f
Taras Mankovski <tarasm@gmail.com>
2016-04-09 19:35:26 -0400

    Working setup with fastboot

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,9 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+    
+    wordpressHost: 'http://104.196.21.75'
   };
 
   if (environment === 'development') {
